5508aeadf4531da02cf5c85ef66ea6d9c0b8d7cb
Djordje Todorovic <djolertrk@gmail.com>
2021-07-19 22:28:52 +0200

    Set up the project
      - Set up the build system (with cmake)
      - Add compile units
      - Add include files

---
+++ src/CodeGen/CodeGen.cpp
@@ -0,0 +1,5 @@
+// === This file contains the implementation of the revLANG constructs. ===
+
+int dummy() {
+  return 0;
+}
---
+++ src/revLANG.cpp
@@ -0,0 +1,10 @@
+// === This file contains an implementaion for the driver that will be used
+// as an interpreter for the revLANG language. ===
+
+#include <iostream>
+
+int main() {
+  std::cout << "=== revLang interpreter ===\n";
+
+  return 0;
+}
